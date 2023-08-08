63f1edf2ed90d308cc5b29f69c58e7d01fb7cabc
ciza99 <cizekmichal99@gmail.com>
2023-08-08 09:28:17 +0200

    feat: hiding preview for secret files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -12,6 +12,7 @@ local M = {
 			local telescope = require("telescope")
 			local actions = require("telescope.actions")
 			local builtin = require("telescope.builtin")
+			local putils = require("telescope.previewers.utils")
 
 			local function telescope_buffer_dir()
 				return vim.fn.expand("%:p:h")
@@ -24,6 +25,30 @@ local M = {
 					sorting_strategy = "ascending",
 					layout_config = {
 						-- prompt_position = "top",
+					},
+					preview = {
+						-- 1) Do not show previewer for certain files
+						filetype_hook = function(filepath, bufnr, opts)
+							-- you could analogously check opts.ft for filetypes
+							local excluded = vim.tbl_filter(function(ending)
+								return filepath:match(ending)
+							end, {
+								"%.env.*",
+							})
+							if not vim.tbl_isempty(excluded) then
+								putils.set_preview_message(bufnr, opts.winid, "Sorry, I am not showing this")
+								return false
+							end
+							return true
+						end,
+						-- 2) Truncate lines to preview window for too large files
+						filesize_hook = function(filepath, bufnr, opts)
+							local path = require("plenary.path"):new(filepath)
+							-- opts exposes winid
+							local height = vim.api.nvim_win_get_height(opts.winid)
+							local lines = vim.split(path:head(height), "[\r]?\n")
+							vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+						end,
 					},
 					mappings = {
 						i = {
